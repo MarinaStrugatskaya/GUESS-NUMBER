9d4a2b179eb48f71afce46e64410355920fb92e3
Marina Strugatskaya <sharoniyamit1@gmail.com>
2024-04-22 09:33:18 +0300

    Add initial commit

---
+++ guess_number.py
@@ -0,0 +1,18 @@
+from random import randint
+
+number = randint(1, 100)
+print('Guess the number!')
+
+while True:
+    guess = int(input('Enter your nimber: '))
+
+    if guess < number:
+        print('Your number is less than hidden! Try again!')
+
+     if guess > number:
+        print('Your number is bigger than hidden! Try again!')
+
+     if guess == number:
+        print('Well done')
+        break
+
